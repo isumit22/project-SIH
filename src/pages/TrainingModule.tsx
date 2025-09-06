dcbd36621be3f623b5b36b7955f3e306be771761
Sumit Chaubey <139735352+isumit22@users.noreply.github.com>
2025-09-06 23:17:18 +0530

    summary

--- src/pages/TrainingModule.tsx
+++ src/pages/TrainingModule.tsx
@@ -29,7 +29,7 @@ const TrainingModule: React.FC = () => {
         'Common mistakes to avoid',
         'Environmental impact of proper segregation'
       ],
-      videoUrl: '/video/video1.mp4'
+      videoUrl: '/public/videos/video1.mp4'
     },
     {
       id: 2,
@@ -45,7 +45,7 @@ const TrainingModule: React.FC = () => {
         'Metal recycling guidelines',
         'Glass disposal best practices'
       ],
-      videoUrl: '/video/video2.mp4'
+      videoUrl: '/public/videos/video2.mp4'
     },
     {
       id: 3,
@@ -61,7 +61,7 @@ const TrainingModule: React.FC = () => {
         'Certified e-waste collection centers',
         'Environmental hazards of improper disposal'
       ],
-      videoUrl: '/video/video3.mp4'
+      videoUrl: '/public/videos/video3.mp4'
     },
     {
       id: 4,
@@ -77,7 +77,7 @@ const TrainingModule: React.FC = () => {
         'Maintaining optimal conditions',
         'Using finished compost in gardening'
       ],
-      videoUrl: '/video/video4.mp4'
+      videoUrl: '/public/videos/video4.mp4'
     }
   ];
 
